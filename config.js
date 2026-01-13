08a4cf26c0c3412ae45bc0420390c4f8727ca9b2
RIAN-AKBAR <rian032008@gmail.com>
2026-01-13 16:44:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 // Supabase configuration
-const SUPABASE_URL = 'https://ydsyaszwpcyoxkimbcfs.supabase.co'; // Replace with your Supabase project URL
-const SUPABASE_ANON_KEY = 'sb_publishable_IF8H7LF9I-7WujAVd6r2Pw_bMDe64Gc'; // Replace with your Supabase anon key
+const NEXT_PUBLIC_SUPABASE_URL = 'https://ydsyaszwpcyoxkimbcfs.supabase.co'
+const NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY = 'sb_publishable_IF8H7LF9I-7WujAVd6r2Pw_bMDe64Gc'
 
 // Initialize Supabase client
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = supabase.createClient(NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY);
+
 
 
