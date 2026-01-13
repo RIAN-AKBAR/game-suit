21ce5d2989bf629b6b2ee04e28ce3fef029d4fb8
RIAN-AKBAR <rian032008@gmail.com>
2026-01-13 18:09:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-// Supabase configuration
+// Supabase Configuration
 const SUPABASE_URL = 'https://ydsyaszwpcyoxkimbcfs.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_IF8H7LF9I-7WujAVd6r2Pw_bMDe64Gc';
